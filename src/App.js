9582a7f4cef51140ab78068e5339f4237ec893e3
ragulhpSPK <ragulhp27@gmail.com>
2023-06-30 15:07:01 +0530

    ghg

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("https://chat-app-server-lime.vercel.app/", {
-  cors: {
-    origin: ["https://chat-app-lemon-eight.vercel.app"],
-  },
-});
+const socket = io.connect("https://chat-app-server-lime.vercel.app");
 
 function App() {
   const [userName, setUserName] = useState("");
